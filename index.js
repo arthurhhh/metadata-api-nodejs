d1fd05754ad4f6ed6155bd592261b7724d2f3ac0
arthurhhh <92421850+arthurhhh@users.noreply.github.com>
2021-12-03 22:15:01 -0800

    Bug fix & Took away some unrelated meta data
    As titled

--- index.js
+++ index.js
@@ -20,21 +20,13 @@ app.get('/', function(req, res) {
 })
 
 app.get('/api/token/:token_id', async function(req, res) {
-  let idInt = parseInt(req.params.token_id) % 6
+  let idInt = parseInt(req.params.token_id) % 5 + 1
   const fakeTokenId = idInt.toString()
-  const person = db[fakeTokenId]
-  const bdayParts = person.birthday.split(' ')
-  const day = parseInt(bdayParts[1])
-  const month = parseInt(bdayParts[0])
   const {name, value} = await token_access(req.params.token_id)
   const data = {
     'name': name,
     'description': '"' + value + '"',
     'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
-      'age': moment().diff(person.birthday, 'years')
     },
     'image': `${HOST}/images/${fakeTokenId}.png`
   }
@@ -44,13 +36,6 @@ app.get('/api/token/:token_id', async function(req, res) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
-
-// returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
 
 function monthName(month) {
   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
